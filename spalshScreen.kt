626a5d9d140cbc3cfbcab5f0d4344dd49a2ff6ea
Tarikul Islam Shykat <96073081+helper-bud@users.noreply.github.com>
2024-02-08 16:59:27 +0600

    Create spalshScreen.kt

---
+++ spalshScreen.kt
@@ -0,0 +1,3 @@
+ Handler(Looper.getMainLooper()).postDelayed({ 
+                findNavController().navigate(R.id.action_splashScreenFragment_to_signInFragment)                                    
+        }, 2500)
