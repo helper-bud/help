61edeab6a0389a9ddea9ff97a26c990d89f43f66
Tarikul Islam Shykat <96073081+helper-bud@users.noreply.github.com>
2024-02-23 00:01:26 +0600

    Create 1. variable.dart

---
+++ 1. flutter/1. Language/1. variable.dart
@@ -0,0 +1,12 @@
+var vs dynamic 
+1. var is once assigned and the data type cant be changed. means if initially we set int value, it will be int the whole time
+2. Dynamic variable type can be changed over time . 
+
+  void main(){
+  var a = 10; // cant c
+  a = 20;
+  a = "erro"; // shows error.
+
+  dynamic b = 20;
+  b = 'bob'; // no error. 
+}
