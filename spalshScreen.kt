bf6d6ae9aa62955c91a2b34d1f977e47cf801dee
Tarikul Islam Shykat <96073081+helper-bud@users.noreply.github.com>
2024-05-04 12:36:04 +0600

    Delete spalshScreen.kt

--- spalshScreen.kt
+++
@@ -1,3 +0,0 @@
- Handler(Looper.getMainLooper()).postDelayed({ 
-                findNavController().navigate(R.id.action_splashScreenFragment_to_signInFragment)                                    
-        }, 2500)
